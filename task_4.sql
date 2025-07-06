cb1e00d4b105fb7482cb63c52ba0215b5c2e00fd
Ndumiso Ngobese <139388688+ndumisodev@users.noreply.github.com>
2025-07-06 20:52:47 +0200

    Update task_4.sql

--- task_4.sql
+++ task_4.sql
@@ -1,19 +1,5 @@
 -- Use the alx_book_store database
 USE alx_book_store;
 
--- Print the full description of the 'Books' table
--- by querying the INFORMATION_SCHEMA.COLUMNS table.
--- This approach avoids using DESCRIBE or EXPLAIN statements.
-SELECT
-    COLUMN_NAME,
-    COLUMN_TYPE,
-    IS_NULLABLE,
-    COLUMN_KEY,
-    COLUMN_DEFAULT,
-    EXTRA
-FROM
-    INFORMATION_SCHEMA.COLUMNS
-WHERE
-    TABLE_SCHEMA = 'alx_book_store' AND TABLE_NAME = 'Books'
-ORDER BY
-    ORDINAL_POSITION;
+-- Print the full description of the 'Books' table using DESCRIBE
+DESCRIBE Books;
