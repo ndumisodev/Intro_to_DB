75428e106e810f8bda50a669d1f8346bb4db0526
Ndumiso Ngobese <139388688+ndumisodev@users.noreply.github.com>
2025-07-06 21:12:26 +0200

    Update task_4.sql

--- task_4.sql
+++ task_4.sql
@@ -1,4 +1,3 @@
-
 USE alx_book_store;
 
 SELECT COLUMN_NAME, COLUMN_TYPE, COLUMN_KEY, EXTRA
@@ -6,27 +5,3 @@ FROM INFORMATION_SCHEMA.COLUMNS
 WHERE TABLE_NAME = 'Books' AND TABLE_SCHEMA = 'alx_book_store';
 
 
-
-
-
-
-
--- Use the alx_book_store database
--- USE alx_book_store;
-
--- Print the full description of the 'Books' table
--- by querying the INFORMATION_SCHEMA.COLUMNS table.
--- This approach avoids using DESCRIBE or EXPLAIN statements.
-
-
--- USE alx_book_store;
-
--- SELECT 
---     COLUMN_NAME, 
---     COLUMN_TYPE, 
---     COLUMN_KEY, 
---     EXTRA
--- FROM 
---     INFORMATION_SCHEMA.COLUMNS
--- WHERE 
---     TABLE_NAME = 'Books' AND TABLE_SCHEMA = 'alx_book_store';
